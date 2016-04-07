d59ae2620b0c2956f6b730341d03f35a9d9ce8d5
Matthew Moore <Matthew@ocicatmedia.com>
2016-04-07 14:51:55 -0400

    Added files via upload

--- compile.py
+++ compile.py
@@ -7,13 +7,14 @@ import re
 	
 	
 class WebSite:
-	v_page = []
+	v_template = ''
 	v_meta = {}
 	v_title = {}	
-	v_nav = []
 	v_url = {}
-	v_cont = {}
-	v_template = ''
+	v_nav = {}
+
+	v_page = {}
+
 		
 	def __init__ (self):
 		
@@ -34,6 +35,10 @@ class WebSite:
 			WebSite.v_meta[i['Meta_Tag']] = i['Meta_Value']
 			WebSite.v_template = WebSite.v_template.replace('<$META_' + i['Meta_Tag'].upper() + '$>',i['Meta_Value'])
 
+		# set SITE TITLE
+		WebSite.v_title['site'] = v_meta['appname']
+		WebSite.v_template = WebSite.v_template.replace('<$TITLE_SITE$>',WebSite.v_title['site'])
+		
 		# retrieve and populate GLOBAL URL values
 		lv_query = "SELECT * FROM ocm_webcompile.default_url"
 		cursor_webcompile.execute(lv_query)
@@ -43,39 +48,32 @@ class WebSite:
 			WebSite.v_url[i['Url_Name']] = i['Url_Value']
 			WebSite.v_template = WebSite.v_template.replace('<$URL_' + i['Url_Name'].upper() + '$>',i['Url_Value'])
 
-		# retrieve GLOBAL NAVIGATION structure
-		lv_query = "SELECT site_section.Section_ID, Section_Title, Nav_Title, Page_Path FROM ocm_webcompile.site_section INNER JOIN ocm_webcompile.site_nav ON site_section.Section_ID = site_nav.Section_ID INNER JOIN ocm_webcompile.site_page ON site_nav.Page_ID = site_page.Page_ID WHERE site_section.Section_PrimNav = 1 ORDER BY site_section.Section_ID"
+		# retrieve NAVIGATION structures
+		lv_query = "SELECT Nav_Title, Section_Name, Page_Path FROM ocm_webcompile.site_nav INNER JOIN ocm_webcompile.site_section ON site_nav.Section_ID = site_section.Section_ID INNER JOIN ocm_webcompile.site_page ON site_nav.Page_ID = site_page.Page_ID ORDER BY site_nav.Section_ID, Nav_Order"
 		cursor_webcompile.execute(lv_query)
 		query_result = cursor_webcompile.fetchall()
 		
 		for i in query_result:
-			WebSite.v_nav.append([i['Section_ID'],i['Nav_Title'],re.sub('^(|(.+?)/)$','\g<1>index',i['Page_Path']) + '.html'])
+			WebSite.v_nav[i['Section_Name']].append([i['Nav_Title'],re.sub('^(|(.+?)/)$','\g<1>index',i['Page_Path']) + '.html'])
 			
+		# construct FOOTER NAVIGATION
+		lv_footnav = ''
+		for i in v_nav['foot']:
+			lv_footnav = lv_footnav + '<li><a href="' + i[1] + '">' + i[0] + '</a></li>'
 		
-
-
-
+		lv_footnav = '<ul>' + lv_footnav + '</ul>'
+		WebSite.v_template = WebSite.v_template.replace('<$NAV_FOOT$>',lv_footnav)
 		
+		# construct STATIC PAGES
+		lv_query = "SELECT Page_Title, Page_Name, Page_Path, Page_MetaDesc, Page_MetaKey, Page_Content, Section_Title FROM ocm_webcompile.site_page INNER JOIN ocm_webcompile.site_section ON site_page.Section_ID = site_section.Section_ID ORDER BY site_section.Section_ID"
+		cursor_webcompile.execute(lv_query)
+		query_result = cursor_webcompile.fetchall()
 		
-		
-			
-			
-
-			
-			
+		for i in query_result:
+			WebSite.v_page[i['Section_Title']].append(WebPage(i))
 
 		cursor_webcompile.close()
 		cnx_webcompile.close()		
-			
-		# WebSite.v_template = WebSite.v_template.replace('<$NAV_FOOT$>',build_nav('foot',''))
-		
-
-		# WebSite.v_template = WebSite.v_template.replace('<$URL_ROOT$>',WebSite.v_url['root'])
-		# WebSite.v_template = WebSite.v_template.replace('<$URL_ASSET$>',WebSite.v_url['asset'])
-
-		# WebSite.v_template = WebSite.v_template.replace('<$TITLE_SITE$>',WebSite.v_title['site'])
-
-		# WebSite.v_template = WebSite.v_template.replace('<$CONTENT_FOOTER$>',WebSite.v_cont['foot'])
 		
 	
 
@@ -83,36 +81,89 @@ class WebSite:
 		
 			
 
+class WebPage:
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	def __init__ (self,pagequery):
+		self.v_template = WebSite.v_template
+		self.v_section = pagequery['Section_Title']
+		
+		self.v_pagetitle = pagequery['Page_Title']
+		self.v_pagename = pagequery['Page_Name']
+		self.v_pagepath = re.sub('^(|(.+?)/)$','\g<1>index',pagequery['Page_Path']) + '.html'
 
+		self.v_metakey = pagequery['Page_MetaKey']
+		self.v_metadesc = pagequery['Page_MetaDesc']
+		self.v_metalink = '<!-- link rel="canonical" -->'
+	
+		self.v_content = pagequery['Page_Content']
+		
+		self.v_sitenav = ''
+		self.v_pagenav = ''
+		
+		# construct SITE NAVIGATION
+		for i in WebSite.v_nav['site']:
+			lv_sitenav = '<a href="' + i[1] + '">' + i[0] + '</a></li>'
+			if (i[0].upper() == self.v_section.upper()):
+				lv_sitenav = '<li class="active">' + lv_sitenav
+			else:
+				lv_sitenav = '<li>' + lv_sitenav
+			self.v_sitenav = self.v_sitenav + lv_sitenav
+			
+		self.v_sitenav = '<ul>' + self.v_sitenav + '</ul>'
+
+		# construct PAGE NAVIGATION
+		for i in WebSite.v_nav[self.v_section]:
+			lv_pagenav = '<a href="' + i[1] + '">' + i[0] + '</a></li>'
+			if (i[1] == self.v_pagepath):
+				lv_pagenav = '<li class="active">' + lv_pagenav
+			else:
+				lv_pagenav = '<li>' + lv_pagenav
+			self.v_pagenav = self.v_pagenav + lv_pagenav
+		if (len(self.v_pagenav)):
+			self.v_pagenav = '<nav><h2 class="WCAG_hide">Page Navigation</h2><ul>' + self.v_pagenav + '</ul></nav>'
+		
+		# inject NAVIGATION CONSTRUCTS
+		self.v_template = self.v_template.replace('<$NAV_SITE$>',self.v_sitenav)
+		self.v_template = self.v_template.replace('<$NAV_PAGE$>',self.v_pagenav)
+
+		# construct BREADCRUMB
+		lv_breadcrumblevel = 1
+		self.v_breadcrumb = '<ol><li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' + WebSite.v_url['root'] + '" itemprop="item"><span itemprop="name">' + WebSite.v_title['site'] + '</span></a><meta itemprop="position" content="' + lv_breadcrumblevel + '"></li>'
+		if (re.search("[^/]+/[^/]+", self.v_pagepath)):
+			lv_breadcrumblevel += 1
+			self.v_breadcrumb = self.v_breadcrumb + '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' + WebSite.v_url['root'] + self.v_pagepath.split('/')[0] + '.html" itemprop="item"><span itemprop="name">' + self.v_section.title() + '</span></a><meta itemprop="position" content="' + lv_breadcrumblevel + '"></li>'
+		lv_breadcrumblevel += 1
+		self.v_breadcrumb = self.v_breadcrumb + '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="' + WebSite.v_url['root'] + self.v_pagepath + '" itemprop="item"><span itemprop="name">' + self.v_pagetitle + '</span></a><meta itemprop="position" content="' + lv_breadcrumblevel + '"></li>'
+		self.v_breadcrumb = self.v_breadcrumb + '<$NAV_BREADCRUMB$></ol>'
+		
+		# inject BREADCRUMB CONSTRUCT
+		self.v_template = self.v_template.replace('<$NAV_BREADCRUMB$>',self.v_breadcrumb)
 		
 		
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		# inject CONTENT
+		self.v_template = self.v_template.replace('<$META_KEYWORD$>',self.v_metakey)
+		self.v_template = self.v_template.replace('<$META_DESCRIPTION$>',self.v_metadesc)
+		self.v_template = self.v_template.replace('<$META_TITLE$>',self.v_pagetitle)
+		self.v_template = self.v_template.replace('<$META_LINK$>',self.v_metalink)
+
+		self.v_template = self.v_template.replace('<$TITLE_PAGE$>',self.v_pagename)
+
+		self.v_template = self.v_template.replace('<$CONTENT$>',self.v_content)
 		
 
-	
 
 		
 
--- sql/webcompile.sql
+++ sql/webcompile.sql
@@ -3,8 +3,6 @@ CREATE DATABASE IF NOT EXISTS ocm_webcompile DEFAULT CHARACTER SET utf8;
 CREATE TABLE IF NOT EXISTS ocm_webcompile.site_section (
 	Section_ID TINYINT(1) UNSIGNED NOT NULL AUTO_INCREMENT,
 	Section_Title VARCHAR(100) NOT NULL,
-	Section_PrimNav TINYINT(1) UNSIGNED,
-	Section_Order TINYINT(1) UNSIGNED,
 	PRIMARY KEY (Section_ID),
 	UNIQUE KEY UN_Section_Title (Section_Title)
 ) ENGINE=InnoDB;
@@ -23,13 +21,15 @@ CREATE TABLE IF NOT EXISTS ocm_webcompile.site_page (
 	UNIQUE KEY UN_Page_Path (Page_Path),
 	UNIQUE KEY UN_Page_Name (Page_Name)
 ) ENGINE=InnoDB;
-	
+
 CREATE TABLE IF NOT EXISTS ocm_webcompile.site_nav (
 	Nav_ID TINYINT(1) UNSIGNED NOT NULL AUTO_INCREMENT,
 	Nav_Title VARCHAR(100) NOT NULL,
+	Nav_Order TINYINT(1) UNSIGNED NOT NULL,
 	Section_ID TINYINT(1) UNSIGNED NOT NULL,
 	Page_ID TINYINT(1) UNSIGNED NOT NULL,
-	PRIMARY KEY (Nav_ID)
+	PRIMARY KEY (Nav_ID),
+	UNIQUE KEY UN_Section_Order (Section_ID,Nav_Order)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS ocm_webcompile.default_meta (
@@ -48,33 +48,19 @@ CREATE TABLE IF NOT EXISTS ocm_webcompile.default_url (
 
 
 
-SELECT Nav_Title, Page_Path 
-
-FROM ocm_webcompile.site_nav 
-
-INNER JOIN ocm_webcompile.site_page 
-ON site_nav.Page_ID = site_page.Page_ID 
-INNER JOIN ocm_webcompile.site_section 
-ON site_nav.Section_ID = site_section.Section_ID
-AND Section_Title = "Foot"
-
- 
 
 
-
-ORDER BY site_section.Section_ID
 
 
 
 
 /*		DEFAULT VALUES FOR OCICATMEDIA.COM		*/
-INSERT INTO ocm_webcompile.site_section (Section_ID, Section_Title, Section_PrimNav, Section_Order) VALUES (1,"Site",0,0),(2,"Foot",0,0),(3,"Home",1,1),(4,"Blog",1,2),(5,"Portfolio",1,3),(6,"About",1,4),(7,"Contact",1,5);
+INSERT INTO ocm_webcompile.site_section (Section_ID, Section_Title) VALUES (1,"site"),(2,"foot"),(3,"home"),(4,"blog"),(5,"portfolio"),(6,"about"),(7,"contact");
 INSERT INTO ocm_webcompile.site_page (Page_ID, Page_Title, Page_Name, Page_Path, Page_MetaDesc, Page_MetaKey, Page_Content, Section_ID) VALUES (1,"OciCat Media","Home","","The homepage of the website for OciCat Media: a high quality media company offerning a multitude of services.","Multimedia, Photography, Drafting, Computer Aided Drafting, CAD, Film, Video, Development, Web Development, Programming","<%%>",3),(2,"OciCat Media Blog","Blog","news","Every blog post and news article pertaining to OciCat Media, its projects, and work are indexed here.","Blog, News","<%BLOG_ARTICLE_INDEX%>",4),(3,"OciCat Media Portfolio","Portfolio","portfolio","All of OciCat Media's previous works are gathered in the portfolio, grouped by various forms of media.","Multimedia, Photography, Picture, Image, Drafting, Computer Aided Drafting, CAD, Film, Video","<%%>",5),(4,"Photography","Photographs","portfolio/photography","The index page of the photography section of OciCat Media's professional portfolio.","Multimedia, Photography, Picture, Image","<%PORTFOLIO_PHOTO_INDEX%>",5),(5,"Drafting","Drafting Projects","portfolio/drafting","The index page of the drafting and CAD section of OciCat Media's professional portfolio.","Multimedia, Drafting, Computer Aided Drafting, CAD","<%PORTFOLIO_CAD_INDEX%>",5),(6,"Video","Videos","portfolio/video","The index page of the film and video section of OciCat Media's professional portfolio.","Multimedia, Film, Video","<%PORTFOLIO_VIDEO_INDEX%>",5),(7,"About","About","about","Read all about OciCat Media, its founder Matthew Moore, and even learn a little about the company's namesake: the Ocicat.","About, About Us, OciCat Media, The Ocicat, Ocicat, Matthew Moore","<%%>",6),(8,"About OciCat Media","About OciCat Media","about/ocicat-media","Read all about OciCat Media, its founding, and how it promises to be a great choice for your multimedia needs.","About, About Us, OciCat Media","<%%>",6),(9,"About Matthew Moore","About Matthew Moore","about/matthew-moore","Read all about the founder of OciCat Media, Matthew Moore.","About, About Us, Matthew Moore","<%%>",6),(10,"About the Ocicat","About the Ocicat","about/ocicat","Read about the origins of this mystifying feline, and the awesome it is made of.","About, About Us, The Ocicat, Ocicat","<%%>",6),(11,"Contact OciCat Media","Contact Us","contact","Find the information you need, to get in touch with OciCat Media here.","Contact, Contact Us, Contact OciCat Media","<%%>",7),(12,"Our Policies","Policies","policy","All of the policies and standards OciCat Media holds itself - and its work - to, gathered in one handy place.","Policy, Policies, Accessibility, Privacy, Discretion, Fair Use","<%%>",2),(13,"Our Licensing Details","Licenses","license","Check here for all of the information regarding our licenses, such as copyright, trademarks, and fair use policies.","License, Licenses, Copyright, Trademark, Fair Use","<%%>",2),(14,"OciCat Media Sitemap","Sitemap","sitemap","The sitemap for OciCatMedia.com","Sitemap","<%SITEMAP%>",2);
-INSERT INTO ocm_webcompile.site_nav (Nav_ID, Nav_Title, Section_ID, Page_ID) VALUES (1,"Home",1,1),(2,"Blog",1,2),(3,"Portfolio",1,3),(4,"About",1,7),(5,"Contact",1,11),(6,"Policies",2,12),(7,"Licenses",2,13),(8,"Sitemap",2,14),(9,"Photography",5,4),(10,"Drafting",5,5),(11,"Video",5,6),(12,"OciCat Media",6,8),(13,"Matthew Moore",6,9),(14,"The Ocicat",6,10);
+INSERT INTO ocm_webcompile.site_nav (Nav_ID, Nav_Title, Nav_Order, Section_ID, Page_ID) VALUES (1,"Home",1,1,1),(2,"Blog",2,1,2),(3,"Portfolio",3,1,3),(4,"About",4,1,7),(5,"Contact",5,1,11),(6,"Policies",1,2,12),(7,"Licenses",2,2,13),(8,"Sitemap",3,2,14),(9,"Photography",1,5,4),(10,"Drafting",2,5,5),(11,"Video",3,5,6),(12,"OciCat Media",1,6,8),(13,"Matthew Moore",2,6,9),(14,"The Ocicat",3,6,10);
 
 INSERT INTO ocm_webcompile.default_meta (Meta_ID, Meta_Tag, Meta_Value) VALUES (1,"language","en-ca"),(2,"appname","OciCat Media"),(3,"author","Matthew Moore"),(4,"keywords","OciCat Media, Multimedia Provider,"),(5,"description",""),(6,"title","OciCat Media");
 INSERT INTO ocm_webcompile.default_url (Url_ID, Url_Name, Url_Value) VALUES (1,"root","http://ocicatmedia.github.io/"),(2,"asset","asset/"),(3,"path","..\wwwroot\\"),(4,"user","http://ocicatmedia.github.io/user/");
-
 
 
 
